67309275c86c67848e12d88682b018af416df94c
Sergei3232 <45172095+Sergei3232@users.noreply.github.com>
2019-12-20 01:43:27 +0300

    step 2 block warning

--- build/linter.js
+++ build/linter.js
@@ -1,22 +1,67 @@
+let referenceSize = null;
+const errorArray = [];
+
 var link = (str)=>{
     
     const jsonPars = JSON.parse(str)
     
     for (let key in jsonPars){
-        console.log("in", key)
-        console.log(typeof jsonPars[key])
-        console.log(jsonPars[key])    
-    }
+        
+        if(key === "content"){
+            // console.log("Найден массив")
+            masArrya(jsonPars[key])   
+        }
     
+    }
+    console.log(errorArray);
 } 
 
+const masArrya = (content, levl = 0)=>{
+
+    for (let i=0; i<content.length; i++){
+        
+        if(content[i].block != undefined && content[i].block === 'text'){
+
+            if(content[i].mods != undefined && content[i].mods.size != undefined){
+                if(referenceSize === null){    
+                    referenceSize = content[i].mods.size;    
+                }else if(content[i].mods.size != undefined && content[i].mods.size !== referenceSize){    
+                    errorArray.push(
+                        {code: "WARNING.TEXT_SIZES_SHOULD_BE_EQUAL",
+                        error: "Тексты в блоке warning должны быть одного размера"});    
+                }    
+            }
+        }
+
+        let testArrya = content[i];
+
+        for (let key in testArrya){
+                
+            if(key === "content"){
+                masArrya(testArrya[key], ++levl)    
+            }   
+        }
+               
+    }  
+     
+}
 
 const json = `{
     "block": "warning",
     "content": [
         {
             "block": "placeholder",
-            "mods": { "size": "m" }
+            "mods": { "size": "m" },
+            "content": [
+                {
+                    "block": "text",
+                    "mods": { "size": "m" }
+                },
+                {
+                    "block": "text",
+                    "mods": { "size": "m" }
+                }
+            ]
         },
         {
             "elem": "content",
