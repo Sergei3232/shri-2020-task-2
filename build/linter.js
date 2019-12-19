77591a1eb2934e90b888a5904d770b62c45b9d65
Sergei3232 <45172095+Sergei3232@users.noreply.github.com>
2019-12-19 20:09:06 +0300

    step 1

--- build/linter.js
+++ build/linter.js
@@ -1,3 +1,37 @@
-export var link = (str)=>{
-
+var link = (str)=>{
+    
+    const jsonPars = JSON.parse(str)
+    
+    for (let key in jsonPars){
+        console.log("in", key)
+        console.log(typeof jsonPars[key])
+        console.log(jsonPars[key])    
+    }
+    
 } 
+
+
+const json = `{
+    "block": "warning",
+    "content": [
+        {
+            "block": "placeholder",
+            "mods": { "size": "m" }
+        },
+        {
+            "elem": "content",
+            "content": [
+                {
+                    "block": "text",
+                    "mods": { "size": "m" }
+                },
+                {
+                    "block": "text",
+                    "mods": { "size": "l" }
+                }
+            ]
+        }
+    ]
+}`;
+
+link(json)
