3c5ad4b7960a0052fc1d59572e32c6b3bfe25ad7
Sergei3232 <45172095+Sergei3232@users.noreply.github.com>
2019-12-23 11:47:05 +0300

    Тестируем вывод и автотесты

--- build/linter.js
+++ build/linter.js
@@ -4,17 +4,39 @@ const errorArray = [];
 var link = (str)=>{
     
     const jsonPars = JSON.parse(str)
-    
     for (let key in jsonPars){
         
         if(key === "content"){
-            // console.log("Найден массив")
+            
             masArrya(jsonPars[key])   
         }
     
-    }
+    } 
+    // for (let key in jsonPars){
+    //     console.log( jsonPars[key])
+    //     console.log( "key",typeof key)
+    //     console.log( "jsonPars[key]",typeof jsonPars[key])
+
+    //     if (key === 'block' && jsonPars[key] === 'warning'){
+    //         parseWarningBlock(jsonPars[key])    
+    //     }
+    
+    // }
     console.log(errorArray);
 } 
+
+// const parseWarningBlock = (jsonPars) =>{
+
+//     for (let key in jsonPars){
+        
+//         if(key === "content"){
+            
+//             masArrya(jsonPars[key])   
+//         }
+    
+//     } 
+
+// }
 
 const masArrya = (content, levl = 0)=>{
 
@@ -26,9 +48,11 @@ const masArrya = (content, levl = 0)=>{
                 if(referenceSize === null){    
                     referenceSize = content[i].mods.size;    
                 }else if(content[i].mods.size != undefined && content[i].mods.size !== referenceSize){    
-                    errorArray.push(
-                        {code: "WARNING.TEXT_SIZES_SHOULD_BE_EQUAL",
-                        error: "Тексты в блоке warning должны быть одного размера"});    
+                    seterrorArray("WARNING.TEXT_SIZES_SHOULD_BE_EQUAL", "Тексты в блоке warning должны быть одного размера")
+                    // errorArray.push(
+                    //     {code: "WARNING.TEXT_SIZES_SHOULD_BE_EQUAL",
+                    //     error: "Тексты в блоке warning должны быть одного размера",
+                    //     });    
                 }    
             }
         }
@@ -44,6 +68,17 @@ const masArrya = (content, levl = 0)=>{
                
     }  
      
+}
+
+const seterrorArray = (code = "", error= "")=>{
+    errorArray.push({
+        code: code,
+        error: error,
+        location: {
+            start: { column: 0, line: 0 },
+            end: { column: 0, line: 0 }
+        }     
+    })        
 }
 
 const json = `{
