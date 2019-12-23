ef272fe5bf43c78bec23af9d280ac871746fc059
Sergei3232 <45172095+Sergei3232@users.noreply.github.com>
2019-12-23 17:55:32 +0300

    test node

--- build/linter.js
+++ build/linter.js
@@ -1,20 +1,88 @@
+const parse = require('json-to-ast');
+
 let referenceSize = null;
 const errorArray = [];
 
-var link = (str)=>{
+const link = (str)=>{
     
-    const jsonPars = JSON.parse(str)
-    for (let key in jsonPars){
+    const settings = {      
+      loc: true,
+      source: 'data.json'
+    };
+    
+    const jsonPars = parse(str, settings);
+    
+    let item = 1;
+    for (const key in  jsonPars.children){
         
-        if(key === "content"){
+    // console.log(item, 'Ключ', jsonPars.children[key]['key']['value'])
+        if (jsonPars.children[key]['value']['value'] === undefined){
+    // console.log(item, 'Значение:', jsonPars.children[key]['value']['children'])
+            const blockWarning = jsonPars.children[key]['value']['children']
+            // console.log(blockWarning[0])
+            let item1 = 1;
+            for(log in blockWarning){
+                console.log(item1, blockWarning[log])
+                console.log(item1++,typeof blockWarning[log])
+                let item12 = 1   
+                for(log1 in blockWarning[log]){
+                    // console.log(item1, item12++, blockWarning[log][log1])    
+                } 
+            }
+            // stepBlock(blockWarning) 
+        }else{
+    // console.log(item, 'Значение', jsonPars.children[key]['value']['value'])
+        }        
+        
+
+        item++
+    }
+}
+
+const stepBlock = (str) =>{
+    let item = 1;
+    console.log(typeof str['type'])
+    console.log(typeof str['children'])
+    console.log(typeof str['loc'])
+    for (key in str['children']){
+        console.log(item, 'Ключ', str[key])
+        console.log(item, 'Тип', typeof str['children'][key])
+        item++
+    //     // console.log('blockWarning')
+    }
+}
+    // console.log('Текущий children', jsonPars.children)
+    // console.log('Текущий loc', jsonPars.loc)
+    // console.log(typeof jsonPars)
+    // for (const element in jsonPars) {
+
+    //     console.log(typeof jsonPars[element]);
+
+    //     // console.log('Ключ', jsonPars[element])
+    //     // console.log('Значение', jsonPars[element])
+    //   }
+    // const jsonPars = JSON.parse(str)
+
+    // for (const element of jsonPars) {
+    //     console.log(element);
+    //   }
+
+    // console.log(typeof jsonPars['block'])
+    // for (let key in jsonPars){
+         
+    //     if(key === 'block' && jsonPars[key] === 'warning'){
+    //         if(key === "content"){
             
-            masArrya(jsonPars[key])   
-        }
+    //             masArrya(jsonPars[key])   
+    //         }
+    //     }
+        
     
-    } 
-    return errorArray
-    // console.log(errorArray);
-} 
+    // } 
+    // console.log(errorArray)
+// } 
+
+
 
 
 const masArrya = (content, levl = 0)=>{
@@ -89,5 +157,10 @@ const json = `{
         }
     ]
 }`;
+ 
+const json1 = `{
+    "block": {"warning": "Test"}
+    
+}`;
 
-export default link
+link(json)
