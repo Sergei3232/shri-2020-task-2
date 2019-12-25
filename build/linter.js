84fbc846a080c3e15ca1feb7a077586c7f682ed3
Sergei3232 <45172095+Sergei3232@users.noreply.github.com>
2019-12-25 18:13:33 +0300

    25.12.2019 button and placeholder

--- build/linter.js
+++ build/linter.js
@@ -1,7 +1,9 @@
 
 let globalObject = null
 
-if (global){
+if (typeof global === 'undefined') {
+    globalObject = window
+} else if (typeof globalThis === 'undefined') {
     globalObject = global
 }
 
@@ -9,10 +11,14 @@ globalObject.link = (str) => {
 
     const parse = require('json-to-ast')
 
-    const typeError = {textSize:false}
+    const typeError = { //Объект куда записываем ключевые моменты при сравннеии...Эталонный размер блока текст и т.д.
+        buttonPosition: 0,
+        textSize: false,
+        placeholder: false
+    }
     const sizeTextBox = []   //Записываем эталонны размер для блока варнинг при каждой отправке блока на проверку будет создаватсья размер
-    const errorArray  = []   //Массив возврата ошибок
-    let locWarning    = null //Позиция текущего блока Варнинг 
+    const errorArray = []   //Массив возврата ошибок
+    let locWarning = null //Позиция текущего блока Варнинг 
 
     const settings = {
         loc: true,
@@ -20,19 +26,19 @@ globalObject.link = (str) => {
     };
     const jsonPars = parse(str, settings)
 
-    const stepBlock=(jsonPars)=>{
+    const stepBlock = (jsonPars) => {
         if (jsonPars['type'] === 'Object') {
             const parsPbject = jsonPars['children']
-    
+
             for (let i = 0; i < parsPbject.length; i++) {
                 if (parsPbject[i]['key']['value'] === 'block') {
                     // Ищем блок Warning
                     if (parsPbject[i]['value']['value'] === 'warning') {
                         sizeTextBox.push(0)
-                        locWarning = parsPbject[i]['loc']  
+                        locWarning = parsPbject[i]['loc']
                         regulationsWarning(parsPbject)
                     }
-    
+
                 } else if (parsPbject[i]['value']['type'] === 'Array') {
                     stepBlock(parsPbject[i]['value'])
                 }
@@ -42,16 +48,16 @@ globalObject.link = (str) => {
             for (let i = 0; i < parsPbject.length; i++) {
                 stepBlock(parsPbject[i])
             }
-        }       
+        }
     }
 
-    const regulationsWarning = (block)=>{
+    const regulationsWarning = (block) => {
         if (block['type'] === undefined) {//При входе тип не будет определен пока не точно
             for (let i = 0; i < block.length; i++) {
                 if (block[i]['key']['value'] === 'content') {
                     regulationsWarning(block[i]['value'])//Начинаем разбор 
                 }
-    
+
             }
         } else if (block['type'] === 'Array') {
             const parsPbject = block['children']
@@ -60,61 +66,75 @@ globalObject.link = (str) => {
             }
         } else if (block['type'] === 'Object') {
             const parsPbject = block['children']
-    
+            // console.log(1, parsPbject[1])
             for (let i = 0; i < parsPbject.length; i++) {
+                // console.log(i,parsPbject[i]['key']['value'], parsPbject[i]['value']['value'])
                 if (parsPbject[i]['key']['value'] === 'block') {
-    
+
                     if (parsPbject[i]['value']['value'] === 'text') {
-    
+
                         let indexBox = i + 1;
-                        while (parsPbject[indexBox] !== undefined && parsPbject[indexBox]['key']['value'] === 'mods') {                       
+                        while (parsPbject[indexBox] !== undefined && parsPbject[indexBox]['key']['value'] === 'mods') {
                             sizeCheck(parsPbject[indexBox])
                             indexBox++
                         }
-    
+
+                    } else if (parsPbject[i]['value']['value'] === 'button') {
+                        // Тут тут записываем позицию button                  
+                        typeError.buttonPosition = parsPbject[i]['loc']['start']['line']
+                    } else if (parsPbject[i]['value']['value'] === 'placeholder') {
+                        if (typeError.buttonPosition !== 0 && typeError.buttonPosition < parsPbject[i]['loc']['start']['line']) {
+                            
+                            if (!typeError.placeholder) {
+                                typeError.placeholder = true
+                                seterrorArray("WARNING.INVALID_BUTTON_POSITION", "Блок placeholder после button")
+                            }
+                        }
+                        console.log('placeholder')
                     }
-    
+
                 } else if (parsPbject[i]['value']['type'] === 'Array') {
                     regulationsWarning(parsPbject[i]['value'])
                 }
             }
-        }    
+        }
     }
 
     const sizeCheck = (boxSize) => {
-    
-        if (!typeError.textSize){
+
+        if (!typeError.textSize) {
             if (boxSize['value']['type'] === 'Object') {
                 const parsPbject = boxSize['value']['children']
-    
+
                 for (let i = 0; i < parsPbject.length; i++) {
-                    if(parsPbject[i]['key']['value']==='size'){
-                        
-                        if(sizeTextBox[sizeTextBox.length-1] === 0){
-                            sizeTextBox[sizeTextBox.length-1] = parsPbject[i]['value']['value'] 
-                        }else if(sizeTextBox[sizeTextBox.length-1]!== parsPbject[i]['value']['value']){
+                    if (parsPbject[i]['key']['value'] === 'size') {
+
+                        if (sizeTextBox[sizeTextBox.length - 1] === 0) {
+                            sizeTextBox[sizeTextBox.length - 1] = parsPbject[i]['value']['value']
+                        } else if (sizeTextBox[sizeTextBox.length - 1] !== parsPbject[i]['value']['value']) {
                             seterrorArray("WARNING.TEXT_SIZES_SHOULD_BE_EQUAL", "Тексты в блоке warning должны быть одного размера")
                             typeError.textSize = true
                         }
-                             
+
                     }
-                   
+
                 }
+            }
         }
-        }
-        
-    
+
+
     }
 
     const seterrorArray = (code = "", error = "") => {
         const location = {
             start: null,
-            end: null}
-        
-        if (locWarning!==null){
-            location.start = {start: { column: locWarning['start']['column'], line: locWarning['start']['line'] }}
-            location.end   = { column: locWarning['end']['column'], line: locWarning['end']['line'] }
-            }
+            end: null
+        }
+
+        if (locWarning !== null) {
+            location.start = { start: { column: locWarning['start']['column'], line: locWarning['start']['line'] } }
+            location.end = { column: locWarning['end']['column'], line: locWarning['end']['line'] }
+        }
         errorArray.push({
             code: code,
             error: error,
@@ -122,149 +142,42 @@ globalObject.link = (str) => {
         })
     }
 
-    stepBlock(jsonPars)    
+    stepBlock(jsonPars)
     return errorArray
 }
 
-// const stepBlock = (jsonPars) => {
-
-//     if (jsonPars['type'] === 'Object') {
-//         const parsPbject = jsonPars['children']
-
-//         for (let i = 0; i < parsPbject.length; i++) {
-//             if (parsPbject[i]['key']['value'] === 'block') {
-//                 // Ищем блок Warning
-//                 if (parsPbject[i]['value']['value'] === 'warning') {
-//                     sizeTextBox.push(0)
-//                     locWarning = parsPbject[i]['loc']  
-//                     regulationsWarning(parsPbject)
-//                 }
-
-//             } else if (parsPbject[i]['value']['type'] === 'Array') {
-//                 stepBlock(parsPbject[i]['value'])
-//             }
-//         }
-//     } else if (jsonPars['type'] === 'Array') {
-//         const parsPbject = jsonPars['children']
-//         for (let i = 0; i < parsPbject.length; i++) {
-//             stepBlock(parsPbject[i])
-//         }
-//     }
-// }
-
-// const regulationsWarning = (block) => {
-//     // console.log(block['type'])
-//     if (block['type'] === undefined) {//При входе тип не будет определен пока не точно
-//         for (let i = 0; i < block.length; i++) {
-//             if (block[i]['key']['value'] === 'content') {
-//                 regulationsWarning(block[i]['value'])//Начинаем разбор 
-//             }
-
-//         }
-//     } else if (block['type'] === 'Array') {
-//         const parsPbject = block['children']
-//         for (let i = 0; i < parsPbject.length; i++) {
-//             regulationsWarning(parsPbject[i])
-//         }
-//     } else if (block['type'] === 'Object') {
-//         const parsPbject = block['children']
-
-//         for (let i = 0; i < parsPbject.length; i++) {
-//             if (parsPbject[i]['key']['value'] === 'block') {
-
-//                 if (parsPbject[i]['value']['value'] === 'text') {
-
-//                     let indexBox = i + 1;
-//                     while (parsPbject[indexBox] !== undefined && parsPbject[indexBox]['key']['value'] === 'mods') {                       
-//                         sizeCheck(parsPbject[indexBox])
-//                         indexBox++
-//                     }
-
-//                 }
-
-//             } else if (parsPbject[i]['value']['type'] === 'Array') {
-//                 regulationsWarning(parsPbject[i]['value'])
-//             }
-//         }
-//     }
-// }
-
-// const sizeCheck = (boxSize) => {
-    
-//     if (!typeError.textSize){
-//         if (boxSize['value']['type'] === 'Object') {
-//             const parsPbject = boxSize['value']['children']
-
-//             for (let i = 0; i < parsPbject.length; i++) {
-//                 if(parsPbject[i]['key']['value']==='size'){
-                    
-//                     if(sizeTextBox[sizeTextBox.length-1] === 0){
-//                         sizeTextBox[sizeTextBox.length-1] = parsPbject[i]['value']['value'] 
-//                     }else if(sizeTextBox[sizeTextBox.length-1]!== parsPbject[i]['value']['value']){
-//                         seterrorArray("WARNING.TEXT_SIZES_SHOULD_BE_EQUAL", "Тексты в блоке warning должны быть одного размера")
-//                         typeError.textSize = true
-//                     }
-                         
-//                 }
-               
-//             }
-//     }
-//     }
-    
-
-// }
-
-// const seterrorArray = (code = "", error = "") => {
-//     const location = {
-//         start: null,
-//         end: null}
-    
-//     if (locWarning!==null){
-//         location.start = {start: { column: locWarning['start']['column'], line: locWarning['start']['line'] }}
-//         location.end   = { column: locWarning['end']['column'], line: locWarning['end']['line'] }
-//         }
-//     errorArray.push({
-//         code: code,
-//         error: error,
-//         location: location
-//     })
-// }
-
 const json = `{
-    "block": "warning",
+    "block": "grid",
+    "mods": {
+        "m-columns": "10"
+    },
+    
     "content": [
         {
-            "block": "placeholder",
-            "mods": { "size": "m" },
+            "block": "grid",
+            "elem": "fraction",
+            "elemMods": {
+                "m-col": "2"
+            },
             "content": [
                 {
-                    "block": "text",
-                    "mods": { "size": "m" }
-                },
-                {
-                    "block": "text",
-                    "mods": { "size": "l" }
+                    "block": "payment"
                 }
             ]
         },
         {
-            "elem": "content",
+            "block": "grid",
+            "elem": "fraction",
+            "elemMods": {
+                "m-col": "8"
+            },
             "content": [
                 {
-                    "block": "text",
-                    "mods": { "size": "m" }
-                },
-                {
-                    "block": "text",
-                    "mods": { "size": "m" }
+                    "block": "offer"
                 }
             ]
         }
     ]
-}`;
+ }`;
 
-
-
-// window.link = 
-// globalObject.link = link(json)
-// globalObject.link(json)
+console.log(globalObject.link(json))
